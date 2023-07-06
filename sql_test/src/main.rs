0a695e3c0b1cf4b26d42efcd27d8d7982b51cca1
John Scott <john.scott17@nhs.net>
2023-07-06 14:11:40 +0100

    Not having luck with sqlx
    The connection string support for sqlx does not appear to include
    data source names, and there is little documentation and examples
    across the board. It would be better to use something that properly
    supports sql server.

--- sql_test/src/main.rs
+++ sql_test/src/main.rs
@@ -8,14 +8,15 @@ async fn main() -> Result<(), sqlx::Error> {
     //  etc.
     let pool = MssqlPoolOptions::new()
         .max_connections(5)
-        .connect("sql connection string").await?;
+        .connect("mssql://@xsw").await
+        .expect("Failed to connect to database");
 
     // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool).await?;
+    // let row: (i64,) = sqlx::query_as("select top 10 * from abi.dbo.vw_apc_sem_001")
+    //     //.bind(150_i64)
+    //     .fetch_one(&pool).await?;
 
-    assert_eq!(row.0, 150);
+    // assert_eq!(row.0, 150);
 
     Ok(())
 }
