f4c3de7908169631a2f3e44c24009cf0d657288e
John Scott <john.scott17@nhs.net>
2023-07-10 11:31:28 +0100

    Updated dependencies

---
+++ synth_data/src/main.rs
@@ -0,0 +1,50 @@
+use datafusion::arrow::record_batch::RecordBatch;
+use datafusion::arrow::array::{ArrayRef, Float32Array, Int32Array};
+use datafusion::parquet::arrow::arrow_writer::ArrowWriter;
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
+use std::sync::Arc;
+
+use datafusion::prelude::*;
+use datafusion::parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder;
+
+use std::fs;
+
+fn save_record_batch(filename: &str, batch: RecordBatch) {
+    let file = fs::File::create(filename).unwrap();
+    let mut writer = ArrowWriter::try_new(file, batch.schema(), None).unwrap();
+    writer.write(&batch).expect("Writing batch");
+    writer.close().unwrap();
+}
+
+fn load_record_batch(filename: &str) -> RecordBatch {
+    let file = fs::File::open(filename).unwrap();
+    let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
+    println!("Converted arrow schema is: {}", builder.schema());
+    let mut reader = builder.build().unwrap();
+    let record_batch = reader.next().unwrap().unwrap();
+    println!("Read {} records.", record_batch.num_rows());
+    record_batch
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let mut rng = ChaCha8Rng::seed_from_u64(3);
+    println!("{}", rng.gen_range(0..100));
+
+    let col_1 = Arc::new(Int32Array::from_iter([1, 2, 3])) as _;
+    let col_2 = Arc::new(Float32Array::from_iter([1., 6.3, 4.])) as _;
+
+    let batch = RecordBatch::try_from_iter([("col1", col_1), ("col_2", col_2)]).unwrap();
+
+    save_record_batch("example.parquet", batch);
+
+    let batch = load_record_batch("example.parquet");
+
+    let ctx = SessionContext::new();
+    let df = ctx.read_batch(batch).expect("Failed to convert batch to dataframe");
+
+    df.show().await?;
+
+    Ok(())
+}
