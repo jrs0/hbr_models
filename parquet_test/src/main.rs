e97c525c0aa9239d99c4293ad2f1ad379321b667
John Scott <john.scott17@nhs.net>
2023-07-10 11:31:18 +0100

    Fixed conflicts between datafusion/arrow/parquet

--- parquet_test/src/main.rs
+++ parquet_test/src/main.rs
@@ -1,14 +1,15 @@
 use std::{fs, path::Path, sync::Arc};
 
 use parquet::{
-    file::{properties::WriterProperties, writer::SerializedFileWriter},
+    file::reader::FileReader,
+    file::{
+        properties::WriterProperties, reader::SerializedFileReader, writer::SerializedFileWriter,
+    },
     schema::parser::parse_message_type,
 };
 
 fn main() {
-
-
-    // 
+    // Write to file
     let path = Path::new("sample.parquet");
 
     let message_type = "
@@ -20,7 +21,7 @@ fn main() {
     let file = fs::File::create(&path).unwrap();
     let mut writer = SerializedFileWriter::new(file, schema, Default::default()).unwrap();
     let mut row_group_writer = writer.next_row_group().unwrap();
-    
+
     while let Some(mut col_writer) = row_group_writer.next_column().unwrap() {
         // ... write values to a column writer
         col_writer.close().unwrap()
@@ -30,4 +31,15 @@ fn main() {
 
     let bytes = fs::read(&path).unwrap();
     assert_eq!(&bytes[0..4], &[b'P', b'A', b'R', b'1']);
+
+    // Read from file
+    if let Ok(file) = fs::File::open(&path) {
+        let reader = SerializedFileReader::new(file).unwrap();
+
+        let parquet_metadata = reader.metadata();
+        assert_eq!(parquet_metadata.num_row_groups(), 1);
+
+        let row_group_reader = reader.get_row_group(0).unwrap();
+        assert_eq!(row_group_reader.num_columns(), 1);
+    }
 }
