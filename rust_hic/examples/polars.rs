0ce7ce1e923a62ef3685a41fc8cddbf1360adcbe
John Scott <john.scott17@nhs.net>
2023-07-24 18:55:42 +0100

    Got working version of mssql to polars

--- rust_hic/examples/polars.rs
+++ rust_hic/examples/polars.rs
@@ -1,8 +1,16 @@
 use connectorx::prelude::*;
 use std::convert::TryFrom;
+use polars::frame::DataFrame;
 
-let mut source_conn = SourceConn::try_from("mssql://XSW-000-SP09/ABI?trusted_connection=true").expect("parse conn str failed");
-let queries = &[CXQuery::from("select top 10 * from abi.dbo.vw_apc_sem_001"), CXQuery::from("select top 12 * from abi.dbo.vw_apc_sem_001")];
-let destination = get_arrow(&source_conn, None, queries).expect("run failed");
+fn main() {
+    let mut source_conn = SourceConn::try_from("mssql://XSW-000-SP09/ABI?trusted_connection=true")
+        .expect("parse conn str failed");
+    let queries = &[
+        CXQuery::from("select top 10 * from abi.dbo.vw_apc_sem_001"),
+        CXQuery::from("select top 12 * from abi.dbo.vw_apc_sem_001"),
+    ];
+    let destination = get_arrow2(&source_conn, None, queries).expect("run failed");
 
-let data = destination.arrow();
+    let df = destination.polars().expect("Should have worked");
+    println!("{df}");
+}
