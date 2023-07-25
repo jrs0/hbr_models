a73c70c8d1f9ca31e98314eef747b1f9d8de6c34
John Scott <john.scott17@nhs.net>
2023-07-25 10:52:20 +0100

    Adding sorting logic

--- rust_hic/examples/code_parser.rs
+++ rust_hic/examples/code_parser.rs
@@ -1,11 +1,10 @@
-use rust_hic::code_parser::ClinicalCodeTree;
+use rust_hic::clinical_code_tree::ClinicalCodeTree;
 use serde_yaml;
 
 fn main() {
     let f = std::fs::File::open("..\\codes_editor\\icd10_example.yaml")
         .expect("Failed to open file");
-    let d: ClinicalCodeTree = serde_yaml::from_reader(f)
-        .expect("Failed to deserialize to Categories");
+    
 
     println!("{:?}", d);
 }
