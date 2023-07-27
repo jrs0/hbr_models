1d1da8109637809509244a9528e48839e27466ed
John Scott <john.scott17@nhs.net>
2023-07-27 14:41:24 +0100

    Fixed load and save file

--- codes_editor/src-tauri/src/main.rs
+++ codes_editor/src-tauri/src/main.rs
@@ -4,43 +4,30 @@
 )]
 
 //use tauri::api::dialog::{save_file, select, Response};
-use tauri::api::dialog::FileDialogBuilder;
-use tauri::State;
-use std::sync::Mutex;
-use std::sync::Arc;
+//use tauri::api::dialog::FileDialogBuilder;
+//use tauri::State;
+//use std::sync::Mutex;
+//use std::sync::Arc;
 
-struct AppState(Arc<Mutex<String>>);
+//struct AppState(Arc<Mutex<String>>);
 
 #[tauri::command]
-fn open_codes_file(app_state: State<AppState>) {
-    let app_state_2 = app_state.clone();
-    FileDialogBuilder::new().pick_file(move |file_path| {
-        if let Some(path) = file_path {
-            let f = std::fs::File::open(path).expect("Error reading file");
-            let d: serde_json::Value = serde_yaml::from_reader(f).expect("Error parsing YAML");   
-            *app_state_2.0.lock().unwrap() = format!("{d}");
-        }
-    });
+fn open_codes_file(file_path: &str) -> String {
+    let f = std::fs::File::open(file_path).expect("Error reading file");
+    let d: serde_json::Value = serde_yaml::from_reader(f).expect("Error parsing YAML");
+    format!("{d}")
 }
 
 #[tauri::command]
-fn save_yaml(top_level_category: serde_yaml::Value) {
-    // let result = match save_file(Some("yaml"), Some("~")) {
-    //     Ok(response) => match response {
-    //         Response::Okay(s) => s,
-    //         _ => return,
-    //     },
-    //     Err(_) => return,
-    // };
-
-    // let s: String =
-    //     serde_yaml::to_string(&top_level_category).expect("Failed converting to string");
-    // std::fs::write(result, s).expect("Failed writing to file")
+fn save_codes_file(file_path: &str, top_level_category: serde_yaml::Value) {
+    let s: String =
+        serde_yaml::to_string(&top_level_category).expect("Failed converting to string");
+    std::fs::write(file_path, s).expect("Failed writing to file")
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![open_codes_file, save_yaml])
+        .invoke_handler(tauri::generate_handler![open_codes_file, save_codes_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
