aaed374b54d8111f48d2f631af339ee999d3c67a
John Scott <john.scott17@nhs.net>
2023-07-27 17:09:58 +0100

    Going to try exposing code lists in R

--- rust_hic/examples/code_parser.rs
+++ rust_hic/examples/code_parser.rs
@@ -1,11 +1,18 @@
-use rust_hic::clinical_code_tree::ClinicalCodeTree;
-use serde_yaml;
+use rust_hic::{clinical_code_tree::ClinicalCodeTree, clinical_code::ClinicalCodeStore};
 
 fn main() {
     let f = std::fs::File::open("..\\codes_editor\\icd10_example.yaml")
         .expect("Failed to open file");
     
-    
+    // Should execute without panic
+    let code_tree = ClinicalCodeTree::from_reader(f);
+
+    let mut code_store = ClinicalCodeStore::new();
+
+    // Get the ACS STEMI codes in the groups defined by the file
+    let acs_stemi_codes = code_tree
+        .codes_in_group(&format!("acs_stemi"), &mut code_store)
+        .expect("Should succeed, code is present");
 
     
 }
