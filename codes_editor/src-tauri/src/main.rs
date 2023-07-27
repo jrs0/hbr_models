fe0dbd710a492eabb696759801a1109f6f1d5ee4
John Scott <john.scott17@nhs.net>
2023-07-27 11:52:57 +0100

    Having difficulty porting to the new FileDialogBuilder

--- codes_editor/src-tauri/src/main.rs
+++ codes_editor/src-tauri/src/main.rs
@@ -1,81 +1,46 @@
-
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
 
-use tauri::api::dialog::{select, save_file, Response};
+//use tauri::api::dialog::{save_file, select, Response};
+use tauri::api::dialog::FileDialogBuilder;
+use tauri::State;
+use std::sync::Mutex;
+use std::sync::Arc;
 
-use clap::Parser;
-use tauri::Manager;
-
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser)]
-struct Cli {
-    /// The pattern to look for
-    name: Option<String>,
-    /// The path to the file to read
-    path: Option<std::path::PathBuf>,
-}
+struct AppState(Arc<Mutex<String>>);
 
 #[tauri::command]
-fn get_yaml() -> String {
-    let result = match select(Some("yaml"), Some("~")) {
-	Ok(response) => match response {
-	    Response::Okay(s) => s,
-	    _ => return String::from("{\"error\": \"other_error\"}")
-	},
-	Err(_) => return String::from("{\"error\": \"dialog_cancelled\"}"),
-    };
-    let f = std::fs::File::open(result).expect("Error reading file");
-    let d: serde_json::Value = serde_yaml::from_reader(f).expect("Error parsing YAML");
-    // let s = d.to_string();
-    
-    format!("{d}")
+fn open_codes_file(app_state: State<AppState>) {
+    let app_state_2 = app_state.clone();
+    FileDialogBuilder::new().pick_file(move |file_path| {
+        if let Some(path) = file_path {
+            let f = std::fs::File::open(path).expect("Error reading file");
+            let d: serde_json::Value = serde_yaml::from_reader(f).expect("Error parsing YAML");   
+            *app_state_2.0.lock().unwrap() = format!("{d}");
+        }
+    });
 }
 
 #[tauri::command]
 fn save_yaml(top_level_category: serde_yaml::Value) {
-    let result = match save_file(Some("yaml"), Some("~")) {
-	Ok(response) => match response {
-	    Response::Okay(s) => s,
-	    _ => return,
-	},
-	Err(_) => return,
-    };
+    // let result = match save_file(Some("yaml"), Some("~")) {
+    //     Ok(response) => match response {
+    //         Response::Okay(s) => s,
+    //         _ => return,
+    //     },
+    //     Err(_) => return,
+    // };
 
-    let s: String = serde_yaml::to_string(&top_level_category)
-	.expect("Failed converting to string");
-    std::fs::write(result, s).expect("Failed writing to file")
+    // let s: String =
+    //     serde_yaml::to_string(&top_level_category).expect("Failed converting to string");
+    // std::fs::write(result, s).expect("Failed writing to file")
 }
 
-
 fn main() {
-
-    let cli = Cli::parse();
-
-    // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {}", name);
-	return;
-    }
-
-    if let Some(path) = cli.path.as_deref() {
-        println!("Value for config: {}", path.display());
-	return
-    }
-    
     tauri::Builder::default()
-        .setup(|app| {
-	    let main_window = app.get_window("main").unwrap();
-	    main_window.eval(&format!(
-		"window.location.replace('icd')"))
-		.expect("Failed to navigate to icd page");
-	    Ok(())
-	})
-        .invoke_handler(tauri::generate_handler![get_yaml, save_yaml])
-	.run(tauri::generate_context!())
-	.expect("error while running tauri application");
-
-    
+        .invoke_handler(tauri::generate_handler![open_codes_file, save_yaml])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
