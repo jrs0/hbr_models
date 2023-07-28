bc636275fcc3353dedb49ff6c7e617b115a1b473
John Scott <john.scott17@nhs.net>
2023-07-28 10:20:51 +0100

    Got a list returned from rust to R

--- rhic/R/extendr-wrappers.R
+++ rhic/R/extendr-wrappers.R
@@ -17,8 +17,11 @@ hello_world <- function() invisible(.Call(wrap__hello_world))
 
 #' Get the clinical codes in a particular code group defined
 #' in a codes file.
+#'
+#' TODO: figure out a good way to hand errors
+#' 
 #' @export
-get_codes <- function(codes_file_path, group) .Call(wrap__get_codes, codes_file_path, group)
+get_codes_in_group <- function(codes_file_path, group) .Call(wrap__get_codes_in_group, codes_file_path, group)
 
 
 # nolint end
--- rhic/src/rust/src/lib.rs
+++ rhic/src/rust/src/lib.rs
@@ -10,9 +10,14 @@ fn hello_world() {
 
 /// Get the clinical codes in a particular code group defined
 /// in a codes file.
+///
+/// The result is 
+/// 
+/// TODO: figure out a good way to hand errors.
+/// 
 /// @export
 #[extendr]
-fn get_codes_in_group(codes_file_path: &str, group: &str) -> Vec<String> {
+fn get_codes_in_group(codes_file_path: &str, group: &str) -> List {
     let f = std::fs::File::open(codes_file_path).expect("Failed to open codes file");
 
     let code_tree = ClinicalCodeTree::from_reader(f);
@@ -22,17 +27,22 @@ fn get_codes_in_group(codes_file_path: &str, group: &str) -> Vec<String> {
         .codes_in_group(&String::from(group), &mut code_store)
         .expect("Should succeed, code is present");
 
-    let code_names: Vec<_> = clinical_code_refs
-        .iter()
-        .map(|code_ref| {
-            let clinical_code = code_store
-                .clinical_code_from(code_ref)
-                .expect("Clinical code should be present");
-            clinical_code.name().clone()
-        })
-        .collect();
+    let mut name = Vec::new();
+    let mut docs = Vec::new();
+    for code_ref in clinical_code_refs {
+        let clinical_code = code_store
+            .clinical_code_from(&code_ref)
+            .expect("Clinical code should be present");
+        name.push(clinical_code.name().clone());
+        docs.push(clinical_code.docs().clone());
+    }
 
-    code_names
+    // Don't be fooled here -- interpret this as
+    // a dictionary or map defined like
+    // {"name": name, "docs": docs}; in R, the
+    // lvalues are strings, but they are "unquoted"
+    // (they are not variables).
+    list!(name = name, docs = docs)
 }
 
 // Macro to generate exports.
@@ -41,5 +51,5 @@ fn get_codes_in_group(codes_file_path: &str, group: &str) -> Vec<String> {
 extendr_module! {
     mod rhic;
     fn hello_world;
-    fn get_codes;
+    fn get_codes_in_group;
 }
