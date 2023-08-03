a202406b67ebbd223de9d284e35fb99e53844912
John Scott <john.scott17@nhs.net>
2023-08-03 14:42:41 +0100

    Model results are way too high
    I think the bleeding outcomes might be the
    wrong
    way round in the factor

--- scripts/prototypes/log_reg.R
+++ scripts/prototypes/log_reg.R
@@ -14,7 +14,7 @@ dataset <- load_dataset("hes_spells_dataset") %>%
     # at source
     drop_na() %>%
     mutate(outcome_12m_bleeding_al_ani = as.factor(outcome_12m_bleeding_al_ani))
-    
+
 
 dataset %>%
     count(outcome_12m_bleeding_al_ani) %>%
@@ -50,9 +50,45 @@ lr_workflow <-
 # Grid of penalties to try
 lr_reg_grid <- tibble(penalty = 10^seq(-4, -1, length.out = 30))
 
-lr_res <- 
-  lr_workflow %>% 
-  tune_grid(val_set,
-            grid = lr_reg_grid,
-            control = control_grid(save_pred = TRUE),
-            metrics = metric_set(roc_auc))
+lr_res <-
+    lr_workflow %>%
+    tune_grid(val_set,
+        grid = lr_reg_grid,
+        control = control_grid(save_pred = TRUE),
+        metrics = metric_set(roc_auc)
+    )
+
+lr_plot <-
+    lr_res %>%
+    collect_metrics() %>%
+    ggplot(aes(x = penalty, y = mean)) +
+    geom_point() +
+    geom_line() +
+    ylab("Area under the ROC Curve") +
+    scale_x_log10(labels = scales::label_number())
+
+lr_plot
+
+top_models <-
+  lr_res %>% 
+  show_best("roc_auc", n = 15) %>% 
+  arrange(penalty) 
+top_models
+
+lr_best <- 
+  lr_res %>% 
+  collect_metrics() %>% 
+  arrange(penalty) %>% 
+  slice(12)
+lr_best
+
+lr_auc <- 
+  lr_res %>% 
+  collect_predictions(parameters = lr_best)
+  
+  
+   %>% 
+  roc_curve(outcome_12m_bleeding_al_ani, .pred_outcome_12m_bleeding_al_ani) %>% 
+  mutate(model = "Logistic Regression")
+
+autoplot(lr_auc)
