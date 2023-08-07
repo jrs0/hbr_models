39e3e0c9ef2050125da5cdc96fef4d0ebd667891
John Scott <john.scott17@nhs.net>
2023-08-07 10:26:23 +0100

    Currently adding the Measurements struct
    Having some issues serializing Option<chrono> to bson

--- rust_hic/examples/patient.rs
+++ rust_hic/examples/patient.rs
@@ -1,12 +1,41 @@
 //! Example showing serializing and deserializing the patient struct
+//! 
+use std::collections::HashMap;
 
-use rust_hic::patient::Patient;
+use rust_hic::{make_pathology_blood, patient::Patient};
 
 fn main() {
+    // Create a new patient with some details
     let mut patient = Patient::default();
     patient.nhs_number = Some(String::from("123456789"));
     patient.age = Some(65);
 
+    // Serialize the patient
     let serialized_patient = bson::to_bson(&patient);
     println!("{:?}", serialized_patient);
+
+    // Make a map of patients from the patient id (trust number)
+    // to a Patient struct
+
+    //let mut patients = HashMap::new();
+
+    // Make synthetic blood test results
+    let df = make_pathology_blood("pathology_blood", 0, 100);
+
+    // Get the columns of interest
+    let subject = df.column("subject").expect("Column should be present");
+    let order_name = df.column("order_name").expect("Column should be present");
+    let test_name = df.column("test_name").expect("Column should be present");
+    let sample_collected = df
+        .column("sample_collected_date_time")
+        .expect("Column should be present");
+    let result_available = df
+        .column("result_available_date_time")
+        .expect("Column should be present");
+
+    // Loop over the series adding values to the Patient map
+    for n in 0..subject.len() {
+        
+    }
+
 }
---
+++ rust_hic/src/patient/measurements.rs
@@ -0,0 +1,48 @@
+use serde::{Serialize, Deserialize};
+use chrono::Utc;
+
+/// Used as a tag to indicate whether a piece of 
+/// information originated in primary or secondary care.
+#[derive(Serialize, Deserialize, Debug)]
+pub enum DataSource {
+    PrimaryCare,
+    SecondaryCare,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum MeasurementValue {
+    Integer(i64),
+    Real(f64),
+    String(String),
+}
+
+#[serde_with::serde_as]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Measurement {
+    /// The value of the measurement at this time point
+    value: MeasurementValue,
+    /// The time the measurement was performed (or when the
+    /// sample was collected from the patient -- i.e. the time
+    /// that the value applied to).
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    #[serde_as(as = "Option<bson::DateTime>")]
+    measurement_date: Option<chrono::DateTime<Utc>>,
+    /// When the measurement was made available, if the result
+    /// required a test that takes time to perform
+    #[serde_as(as = "Option<bson::DateTime>")]
+    measurement_available: Option<chrono::DateTime<Utc>>,
+    /// Whether the measurement came from a primary or secondary
+    /// care data source.
+    data_source: Option<DataSource>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MeasurementHistory {
+    /// The name of the measurement
+    measurement_name: String,
+    /// The unit of measurement
+    measurement_unit: String,
+    /// The list of measurements results with times
+    timeseries: Vec<Measurement>,
+}
+
