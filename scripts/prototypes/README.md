ba2e3f666123933d8697a6698a843650154c63c1
John Scott <john.scott17@nhs.net>
2023-08-10 13:41:57 +0100

    Calculated time to episode

--- scripts/prototypes/README.md
+++ scripts/prototypes/README.md
@@ -16,7 +16,7 @@ install.packages("rextendr", repos = "https://pbil.univ-lyon1.fr/CRAN/")
 The prototype R scripts in this folder are written using a consistent method in an attempt to make them easier to understand and check. The following guidelines have been followed:
 
 * As far as possible, the same column names have been used for the same things across the scripts. Sometimes this entails renaming something in a script that looks pointless (e.g. "spell_time" to "spell_date"), but it was thought that consistency across the scripts would be more helpful than consistency with the original column names in the data sources.
-* Tables used as intermediate calculations have been kept as small as possible, and as normalised as possible (a single key links all the tables, and tables contain ideally non-overlapping non-primary-key columns). The following pattern has been adopted for calculating intermediate tables:
+* Tables used as intermediate calculations have been kept as small as possible (i.e. low number of columns), and as normalised as possible (a single key links all the tables, and tables contain ideally non-overlapping non-primary-key columns). The following pattern has been adopted for calculating intermediate tables:
     * Left-join together previous tables to obtain the information required
     * Perform calculations, and then select only the columns that provide the new information, discarding those that duplicate information in previous tables. These steps are often combined using `transmute`, which performs a mutate-and-select in the same step. This attempts to make the new table independent of previous ones.
 * Any column that is used as a primary key for joining ends in "_id". No other column name ends in "_id".
--- scripts/prototypes/hic_arc_hbr.R
+++ scripts/prototypes/hic_arc_hbr.R
@@ -41,18 +41,24 @@ raw_episodes_data <- dplyr::tbl(con, episodes_id) %>%
         spell_identifier,
         # Taking hospital arrival time as spell start time for the purpose
         # of calculating time-to-subsequent events
-        arrival_dt_tm, 
+        arrival_dt_tm,
         episode_start_time,
     ) %>%
     rename(
         episode_id = ID,
+        patient = subject,
         spell_id = spell_identifier,
         spell_start_date = arrival_dt_tm,
         episode_start_date = episode_start_time,
     ) %>%
-    filter(!is.na(subject)) %>%
+    filter(!is.na(patient)) %>%
     filter(spell_start_date > start_date, spell_start_date < end_date) %>%
     collect()
+
+# Mapping from episode_id to patient. Required later for joining
+# episodes together from different tables by patient.
+patients <- raw_episodes_data %>%
+    select(episode_id, patient)
 
 diagnoses_id <- dbplyr::in_catalog("HIC_COVID_JS", "dbo", "cv_covid_episodes_diagnosis")
 raw_diagnoses_data <- dplyr::tbl(con, diagnoses_id) %>%
@@ -126,7 +132,7 @@ episode_diagnoses_and_procedures <- raw_episodes_data %>%
     select(episode_id) %>%
     left_join(raw_clinical_codes, by = "episode_id")
 
-# Count up instances of different code groups inside each 
+# Count up instances of different code groups inside each
 # episode
 code_group_counts <- episode_diagnoses_and_procedures %>%
     # Remove the dot from the codes and convert to lower case to compare with the code
@@ -159,7 +165,7 @@ idx_episodes <- code_group_counts %>%
     # Pick only the first episode of each spell
     arrange(episode_start_date) %>%
     group_by(spell_id) %>%
-    slice_head(n=1) %>%
+    slice_head(n = 1) %>%
     ungroup() %>%
     filter(mi_schnier_count > 0 | pci_count > 0) %>%
     transmute(
@@ -173,7 +179,7 @@ idx_episodes <- code_group_counts %>%
 # performed in a subsequent episode -- there is no point trying to correct
 # for it here. That is for a future version of the script.
 idx_episode_info <- idx_episodes %>%
-    left_join(code_group_counts, by = c("idx_episode_id"="episode_id")) %>%
+    left_join(code_group_counts, by = c("idx_episode_id" = "episode_id")) %>%
     # Record whether the index event is PCI or conservatively managed (ACS).
     # Record STEMI and NSTEMI as separate columns to account for possibility
     # of neither (i.e. no ACS).
@@ -183,4 +189,101 @@ idx_episode_info <- idx_episodes %>%
         stemi = (mi_stemi_schnier_count > 0),
         nstemi = (mi_nstemi_schnier_count > 0),
         mi = (mi_schnier_count > 0)
+    )
+
+####### COMPUTE COUNT OF PREVIOUS DIAGNOSES AND PROCEDURES #######
+
+# The first step is getting the time from every index event to
+# every other spell (for each patient).
+
+# The first step is knowing the time of the index events, which is
+# obtained by first getting the dates/times of all episodes
+episode_dates_by_patient <- raw_episodes_data %>%
+    select(episode_id, patient, episode_start_date)
+
+# Do the same for the index episodes
+idx_dates_by_patient <- idx_episodes %>%
+    left_join(raw_episodes_data, by = c("idx_episode_id" = "episode_id")) %>%
+    transmute(
+        idx_episode_id,
+        patient,
+        # The spell start date is being used as the index date for the
+        # purpose of this script.
+        idx_date = spell_start_date
+    )
+
+# Join all the episodes' dates back on by patient to get each patient's
+# index events paired up with all their other episodes.
+# Note that this table has negative times for episodes before the index.
+time_from_index_to_episode <- idx_dates_by_patient %>%
+    # Expect many-to-many because the same patient could have
+    # multiple index events.
+    left_join(episode_dates_by_patient,
+        by = "patient", relationship = "many-to-many"
+    ) %>%
+    # Remove the rows which map the index event back to itself
+    filter(idx_episode_id != episode_id) %>%
+    # Calculate the time from index to episode
+    transmute(
+        idx_episode_id,
+        episode_id,
+        index_to_episode_time = episode_start_date - idx_date
+    )
+
+    
+
+
+a <- raw_episodes_data %>%
+    left_join(patients, by = c("idx_episode_id" = "episode_id"))
+
+code_group_counts_by_patient <- code_group_counts %>%
+    left_join(patients, by = "episode_id")
+
+
+episode_time_differences <- idx_epsiodes_by_patient %>%
+    # For each index event, join all other spells that the patient had.
+    # Expect many-to-many because the same patient could have multiple index events.
+    left_join(
+        code_group_counts_with_nhs_number,
+        by = "nhs_number", relationship = "many-to-many"
+    ) %>%
+    # Join on the spell data to the other spells
+    left_join(spell_data, by = c("other_spell_id" = "spell_id")) %>%
+    # Positive time difference for after, negative for before
+    transmute(
+        spell_id,
+        other_spell_id,
+        spell_time_difference = spell_start_date - index_date
+    )
+
+# This table contains the total number of each diagnosis and procedure
+# group in a period before the index event. This could be the previous
+# 12 months, excluding the month before the index event (to account for
+# lack of coding data in that period)
+max_period_before <- lubridate::dyears(1) # Limit count to previous 12 months
+min_period_before <- lubridate::dmonths(1) # Exclude month before index (not coded yet)
+
+counts_before_index <- spell_time_differences %>%
+    # Add a mask to only include the spells in a particular window before the
+    # index event (up to one year before, excluding the month before the index event
+    # when data will not be available). Need to negate the time difference because
+    # spells before the index have negative time.
+    mutate(spell_valid_mask = if_else(
+        (-spell_time_difference) > min_period_before &
+            (-spell_time_difference) <= max_period_before,
+        0,
+        1
+    )) %>%
+    # Join the count information
+    left_join(code_group_counts, by = c("other_spell_id" = "spell_id")) %>%
+    # Do all operations per patient (and per index event for patients with multiple index events)
+    group_by(spell_id) %>%
+    # Sum up the counts in the valid window. By multipling the count by the valid flag (0 or 1),
+    # it is only included if it came from a spell in the valid window.
+    summarise(
+        bleeding_al_ani_count_before = sum(bleeding_al_ani_count * spell_valid_mask),
+        mi_schnier_count_before = sum(mi_schnier_count * spell_valid_mask),
+        mi_stemi_schnier_count_before = sum(mi_stemi_schnier_count * spell_valid_mask),
+        mi_nstemi_schnier_count_before = sum(mi_nstemi_schnier_count * spell_valid_mask),
+        pci_count_before = sum(pci_count * spell_valid_mask),
     )
