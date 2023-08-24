e2f63a4473b267d3a6a8eae759481c733bd301f3
John Scott <john.scott17@nhs.net>
2023-08-24 15:55:57 +0100

    Moved current file to _new and checked out old one
    Want to compare the scripts to find the discrepancy

--- scripts/prototypes/hes_episodes_umap_new.py
+++ scripts/prototypes/hes_episodes_umap_new.py
@@ -8,6 +8,8 @@ import os
 os.chdir("scripts/prototypes")
 
 import hes
+import sparse_encode as spe
+
 import importlib
 import numpy as np
 from sklearn.datasets import load_digits
@@ -18,68 +20,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import umap
+import umap.plot
 from sklearn.preprocessing import OneHotEncoder
 import re
 
 importlib.reload(hes)
+importlib.reload(spe)
 
-####### FETCH RAW SPELL DATA #######
-
-# Polars is slightly slower than pandas here, but polars
-# returns the nhs_number column as an int not a float (that
-# bigint problem again), so preferring polars for now. Both
-# queries are really slow compared to R -- not sure why yet.
 raw_data = hes.get_spells_hes_pandas()
-# pl_data = hes.get_spells_hes_polars()
-# raw_spells = pl_data.to_pandas()
 
-####### CONVERT CODE COLUMNS TO DUMMIES #######
-
-# Replace empty codes ("") with NaN, so that they are
-# ignored in the conversion to dummies
 cols_to_remove = ["nhs_number", "spell_start_date", "spell_end_date"]
 df = raw_data.replace("", np.nan).drop(columns=cols_to_remove, axis=1)
 
 age_and_gender = df[["spell_id", "age", "gender"]]
 
-pattern = re.compile("(diagnosis|procedure)")
-code_cols = [s for s in df.columns if pattern.search(s)]
-index_cols = ["spell_id"]
+long_codes = hes.convert_codes_to_long(df)
 
-def normalise_code(code):
-    '''
-    Remove all whitespace and any dot character,
-    and convert characters in the code to lower case.
-    '''
-    alpha_num = re.sub(r'\W+', '', code)
-    return alpha_num.lower()
-
-# Pivot all the diagnosis and procedure codes into one
-# columns. Consider https://stackoverflow.com/questions/47684961/
-# melt-uneven-data-in-columns-and-ignore-nans-using-pandas
-# for speed.
-long_codes = pd.melt(df, id_vars=index_cols, value_vars=code_cols).dropna()
-long_codes.value = long_codes.value.apply(normalise_code)
-# Prepend icd10 or opc4 to the codes to indicate which are which
-# (because some codes appear in both ICD-10 and OPCS-4)
-pattern = re.compile("diagnosis")
-diagnosis_or_procedure = ["icd10_" if pattern.search(s) else "opcs4_" for s in long_codes.variable]
-long_codes["full_code"] = diagnosis_or_procedure + long_codes.value
-long_codes["position"] = long_codes["variable"].replace("(diagnosis|procedure)_", "", regex = True).astype(int)
-long_codes = long_codes.drop(columns=["variable", "value"])
-
-# Only keep the two 3 diagnosis and procedure codes, under the
-# assumption that the others may contribute more noise than
-# structure, or that the top codes may contain the most
-# important information
-long_codes = long_codes[long_codes.position < 3]
+#highest_priority_position = long_codes.groupby(["spell_id", "full_code"]).position.transform(min)
+#long_codes_dedup = long_codes[long_codes.position == highest_priority_position]
+long_codes_dedup = long_codes.groupby(["spell_id", "full_code"]).min().reset_index()
 
 # Map the position onto the following linear scale: primary diagnosis
 # is 24, through secondary_diagnosis_23 is 1 (same for procedure). The
 # intention is to later create a linear scale where a higher number
 # means a higher priority diagnosis or procedure, and the value 0 is
 # reserved for diagnosis or procedure not present
-#long_codes.position = 24 - long_codes.position
+linear_position = hes.make_linear_position_scale(long_codes_dedup, 23)
 
 # It is too memory-intensive to just encode all the values
 # in one go. Instead, filter the low-frequency codes first,
@@ -89,14 +55,27 @@ long_codes = long_codes[long_codes.position < 3]
 # the get_dummies call. There is also a mistake here -- you
 # will drop spells that have no common codes, which might
 # be a mistake.
-counts = long_codes.full_code.value_counts() / len(long_codes)
+counts = linear_position.full_code.value_counts() / len(linear_position)
 most_frequent_codes = counts.head(1000).index.to_list()
-reduced_codes = long_codes[long_codes.full_code.isin(most_frequent_codes)]
+reduced_codes = linear_position[linear_position.full_code.isin(most_frequent_codes)]
+
+# Calculate a sparse encoded representation of the codes where
+encoded, spell_ids = spe.encode_sparse(reduced_codes)
+
+# Get the list of ages in the same order as the encoded data
+ordered_spells = pd.DataFrame({"spell_id": spell_ids})
+age = ordered_spells.merge(age_and_gender, on = "spell_id")
+
+# Only keep the two 3 diagnosis and procedure codes, under the
+# assumption that the others may contribute more noise than
+# structure, or that the top codes may contain the most
+# important information
+top_three_codes = long_codes[long_codes.position < 3]
 
 # There is an issue where the same code can show up in different
 # positions. Pick the smallest position (higher priority).
 # TODO figure out what is going on here
-#reduced_codes = reduced_codes.groupby(["spell_id", "full_code"]).min().reset_index()
+linear_position_dedup = linear_position.groupby(["spell_id", "full_code"]).min().reset_index()
 
 # Create dummy variables for all the different codes
 #
@@ -110,7 +89,7 @@ encoded = pd.get_dummies(reduced_codes, columns=["full_code"]).groupby("spell_id
 # instead of just a TRUE/FALSE. The value after this pivot is the
 # linear diagnosis/procedure scale from 1 (last secondary) to 24
 # (primary), with NA when the code is not present in the spell.
-#encoded = reduced_codes.pivot(index = "spell_id", columns = "full_code", values = "position")
+encoded = linear_position.pivot(index = "spell_id", columns = "full_code", values = "position")
 # Replace NA with 0 to indicate no code
 #encoded = encoded.fillna(0)
 
@@ -150,9 +129,24 @@ full_encoded = age_and_gender.join(encoded).fillna(False)
 #   (spells) are considered different according to how many of their
 #   clinical codes differ -- this is the Hamming distance.
 
+mapper = umap.UMAP(metric='euclidean', random_state=1, verbose = True)
+res = mapper.fit(encoded)
+
+umap.plot.points(res, values=age.age, theme='viridis')
+plt.show()
+
+
+embedding = mapper.fit_transform(encoded)
+plt.scatter(
+    embedding[:, 0],
+    embedding[:, 1])
+plt.gca().set_aspect('equal', 'datalim')
+plt.title('UMAP projection of HES spell codes', fontsize=24)
+plt.show()
+
 # 2D embedding
 fit = umap.UMAP(
-    n_neighbors = 15,
+    n_neighbors = 50,
     min_dist = 0.1,
     n_components = 2,
     #metric = "euclidean"
