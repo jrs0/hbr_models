16206568442e17dc7710dcf81e7f7197b5bd4a71
John Scott <john.scott17@nhs.net>
2023-09-01 10:19:18 +0100

    Added python parser class. Getting rust dependency problems

--- py_hic/python/py_hic/clinical_codes.py
+++ py_hic/python/py_hic/clinical_codes.py
@@ -6,33 +6,48 @@ from py_hic import _lib_name
 import os
 import pandas
 
+
 def get_groups_in_codes_file(codes_file_path):
-    '''
+    """
     Get the list of valid group names defined in a codes file
-    '''
+    """
     if not os.path.exists(codes_file_path):
         raise ValueError(f"The codes file '{codes_file_path}' does not exist")
-    
+
     return _lib_name.rust_get_groups_in_codes_file(codes_file_path)
 
 
-def get_codes_in_group(codes_file_path, group): 
-   '''
-   Get a pandas dataframe of all the docs in a particular group
-   defined in a codes file.
+def get_codes_in_group(codes_file_path, group):
+    """
+    Get a pandas dataframe of all the docs in a particular group
+    defined in a codes file.
 
-   The datafrane contains two columns: "name", for the clinical code
-   name (e.g. "I22.1"); and "docs, for the description of the code
-   (e.g. "Subsequent myocardial infarction of inferior wall"). 
-   The group is defined by the codes file (e.g. icd10.yaml), and
-   code groups can be edited using the codes editor program. 
-   '''
-   
-   # This will also check if the codes file exists
-   valid_groups = _lib_name.rust_get_groups_in_codes_file(codes_file_path)
+    The datafrane contains two columns: "name", for the clinical code
+    name (e.g. "I22.1"); and "docs, for the description of the code
+    (e.g. "Subsequent myocardial infarction of inferior wall").
+    The group is defined by the codes file (e.g. icd10.yaml), and
+    code groups can be edited using the codes editor program.
+    """
 
-   if not group in valid_groups:
-      raise ValueError(f"code group '{group}' is not present in codes file '{codes_file_path}'")
-   
-   return pandas.DataFrame(_lib_name.rust_get_codes_in_group(codes_file_path, group))
+    # This will also check if the codes file exists
+    valid_groups = _lib_name.rust_get_groups_in_codes_file(codes_file_path)
 
+    if not group in valid_groups:
+        raise ValueError(
+            f"code group '{group}' is not present in codes file '{codes_file_path}'"
+        )
+
+    return pandas.DataFrame(_lib_name.rust_get_codes_in_group(codes_file_path, group))
+
+
+class ClinincalCodeParser:
+    def __init__(diagnosis_codes_file_path, procedure_codes_file_path):
+        """
+        Create a new code parser for diagnosis and procedure codes using
+        the code trees provided in the two file paths
+        """
+        self._parser = _lib_name.RustClinicalCodeParser(
+            diagnosis_codes_file_path, procedure_codes_file_path
+        )
+
+    def 
