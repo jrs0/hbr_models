8380f74e3d4ca4b32a0d181ca930bf666cf12f02
John Scott <john.scott17@nhs.net>
2023-09-07 11:25:19 +0100

    Added some guidance on preprocessing features

--- scripts/prototypes/fit.py
+++ scripts/prototypes/fit.py
@@ -2,12 +2,53 @@
 #
 # This file contains the process referred to in
 # stability.py as D, which takes as input a training
-# set (X_train, y_train), and outputs a single fitted
-# model (or sklearn pipeline eventually). It may use
-# any methods it desires, but must produce an object
-# capable of preprocessing/transforming a test set in
-# the same way as the training set was transformed.
+# set (X, y), and outputs a scikit-learn pipeline. 
 #
+# The feature matrix X is a numerical array where
+# columns are predictors (features) and rows are
+# samples corresponding to an outcome in the vector
+# y. As far as possible, X should be preprocessed
+# before handing it over to this function. However,
+# this preprocessing must _not_ include any information
+# from the testing set. More generally, we only allow 
+# preprocessing that applies an operation to each 
+# sample of the dataset that is:
+# 
+# - local, meaning it only involves values from the
+#   given row of X.
+# - determinstic, meaning it does not involve any
+#   elements of randomness.
+# - non-parametrised, meaning the preprocessing does
+#   not depend on parameters which may need to be 
+#   tuned over.
+#
+# These conditions are stronger than required to
+# prevent leakage of test data into training data,
+# but are designed to push non-trivial preprocessing
+# into the model development pipeline where it can
+# be tuned and consistently applied to training and
+# testing data.
+# 
+# Examples of preprocessing that can be performed on
+# X before passing it to the fit functions:
+#
+# - Creating a feature by counting the number of 
+#   diagnosis codes before the index event. This
+#   is a local (to the index event) operation 
+#   which is non-parametric and deterministic.
+# - Creating features which are deterministic 
+#   functions of the index event (for example, whether
+#   the index MI is STEMI or NSTEMI).
+# 
+# Examples of preprocessing that must not be performed
+# on X before passing it to the fit functions
+#
+# - Any imputing of data which involves a global 
+#   computation (such as imputing an age column based
+#   on the mean)
+# - Any dimension reduction (such as UMAP) that depends
+#   on tuning parameters and is also a global operation.
+# - Any centering and scaling, which is a global operation.
 #
 
 from sklearn.linear_model import LogisticRegression
